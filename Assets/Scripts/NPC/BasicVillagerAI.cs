13caac3199703de23e4bbb98f820b6d816aff3f0
snajera531 <salemnajera@gmail.com>
2021-11-15 20:24:24 -0700

    npc wandering

--- Assets/Scripts/NPC/BasicVillagerAI.cs
+++ Assets/Scripts/NPC/BasicVillagerAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class BasicVillagerAI : MonoBehaviour
+public class BasicVillagerAI : MonoBehaviour
 {
     public enum eColor
     {
@@ -17,4 +17,15 @@ public abstract class BasicVillagerAI : MonoBehaviour
     public bool FacingRight { get; set; }
     public bool Paused { get; set; }
     public Rigidbody2D Rb { get; set; }
+
+    //wandering functionality
+    //npc wanders between the two nodes
+    public GameObject Node1 { get; set; }
+    public GameObject Node2 { get; set; }
+    public int walkTimer = 10;
+
+    private void Update()
+    {
+        
+    }
 }
